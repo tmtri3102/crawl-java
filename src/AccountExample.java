55e53b77802b572836d65556bf78fb1020f2d80a
tmtri3102 <tmtri310251@gmail.com>
2024-11-04 14:04:37 +0700

    Validate

--- src/AccountExample.java
+++ src/AccountExample.java
@@ -1,12 +1,26 @@
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-public class AccountExample {
-    private final String CRTERIA = "^[_a-z0-9]{6,}$";
-    public AccountExample() {}
-    public boolean validateAccount(String account) {
-        Pattern pattern = Pattern.compile(CRTERIA);
-        Matcher  matcher = pattern.matcher(CRTERIA);
-        return matcher.matches();
-    }
-}
+//import java.io.IOException;
+//import java.io.InputStreamReader;
+//import java.net.MalformedURLException;
+//import java.net.URL;
+//import java.util.Scanner;
+//import java.util.regex.Matcher;
+//import java.util.regex.Pattern;
+//
+//public class AccountExample {
+//    public static void main(String[] args) {
+//        try {
+//            URL url = new URL("https://www.google.com");
+//            Scanner scanner = new Scanner(new InputStreamReader(url.openStream()));
+//            scanner.useDelimiter("\\Z");
+//            String content = scanner.next();
+//            // close scanner
+//            scanner.close();
+//            // remove all new line
+//            content = content.replaceAll("\\n+", "");
+//        }
+//        catch(IOException e){
+//            e.printStackTrace();
+//        }
+//    }
+//
+//}
--- src/AccountTest.java
+++ src/AccountTest.java
@@ -1,17 +1,17 @@
-public class AccountTest {
-    private static final String[] validAcc = {"123abc_", "_abc123", "______", "123456", "abcdefgh"};
-
-    private static final String[] invalidAcc = {".@", "12345", "1234_", "abcde"};
-
-    public static void main(String[] args) {
-        AccountExample example = new AccountExample();
-        for (String acc : validAcc) {
-            boolean isValid = example.validateAccount(acc);
-            System.out.println("Account is " + acc + " isValid = " + isValid);
-        }
-        for (String acc : invalidAcc) {
-            boolean isValid = example.validateAccount(acc);
-            System.out.println("Account is " + acc + " isValid = " + isValid);
-        }
-    }
-}
+//public class AccountTest {
+//    private static final String[] validAcc = {"123abc_", "_abc123", "______", "123456", "abcdefgh"};
+//
+//    private static final String[] invalidAcc = {".@", "12345", "1234_", "abcde"};
+//
+////    public static void main(String[] args) {
+//        AccountExample example = new AccountExample();
+//        for (String acc : validAcc) {
+//            boolean isValid = example.validateAccount(acc);
+//            System.out.println("Account is " + acc + " isValid = " + isValid);
+//        }
+//        for (String acc : invalidAcc) {
+//            boolean isValid = example.validateAccount(acc);
+//            System.out.println("Account is " + acc + " isValid = " + isValid);
+//        }
+//    }
+//}
